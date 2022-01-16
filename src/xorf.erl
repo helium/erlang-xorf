59243b8772c754fd9e416d7714759f651d2ce4c6
Marc Nijdam <marc_nijdam@mac.com>
2022-01-16 08:24:54 -0600

    Add edoc

--- src/xorf.erl
+++ src/xorf.erl
@@ -20,6 +20,11 @@
     contains/2
 ]).
 
+%% @doc Create a new filter given a unique list of 64 bit integers.
+%%
+%% Constructs an xor or binary fuse fitler with the given number of bits per
+%% entry for the list of provided keys. Note that providing a non unique list of
+%% integers may result in undefined behavior.
 -spec new(FilterType :: filter_type(), Entries :: [filter_entry()]) ->
     {ok, filter()} | {error, any()}.
 new({binary_fuse, Size}, Keys) ->
@@ -27,16 +32,25 @@ new({binary_fuse, Size}, Keys) ->
 new({exor, Size}, Keys) ->
     xorf_nif:xor_new(Size, Keys).
 
+%% @doc Convert a a filter to it's binary form
 -spec to_bin(Filter :: filter()) -> {ok, binary()} | {error, any()}.
 to_bin(Filter) ->
     xorf_nif:to_bin(Filter).
 
+%% @doc Create a filter from a binary.
+%%
+%% The type of filter is not stored as part of the binary
 -spec from_bin(Type :: filter_type(), Binary :: binary()) -> {ok, filter()} | {error, any()}.
 from_bin({binary_fuse, Size}, Binary) ->
     xorf_nif:bf_from_bin(Size, Binary);
 from_bin({exor, Size}, Binary) ->
     xorf_nif:xor_from_bin(Size, Binary).
 
+%% @doc Check membership in a filter
+%%
+%% Returns true if the given integer entry is a mamber of the filter. Note that
+%% false positives are possible in xor filters, but become less likely with the
+%% number of bits used per entry during filter creation.
 -spec contains(filter(), filter_entry()) -> boolean().
 contains(Filter, Key) ->
     xorf_nif:contains(Filter, Key).
